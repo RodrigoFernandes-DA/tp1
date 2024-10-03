cb9a927763e80b49719175cc9eeb4350e1b9cd4d
Rodrigo Fernandes <fernandes.rcc@gmail.com>
2024-10-03 10:11:58 +0200

    Change 'ça va' for 'allez-vous'

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld {
     public static void main ( String [] args ) {
 
         System . out . println (" Hello World !" );
-        System . out . println (" Comment ça va ?" );
+        System . out . println (" Comment allez-vous ?" );
         System . out . println (" Goodbye !" );
 
     }
